c14b075ae8d0e2601f841639269e633a1d9b3957
Mahadeva1437 <mahanteshsb460@gmail.comMantu777@git config --global user.email mahanteshsb460@gmail.com>
2024-12-27 16:59:21 +0530

    Latest code updated

--- src/test/java/qa/test/fitpeo/testcases/AlertHandle.java
+++ src/test/java/qa/test/fitpeo/testcases/AlertHandle.java
@@ -26,14 +26,12 @@ public class AlertHandle {
 
         // Navigate to the login page
         driver.get("https://field-tracking-dev.empmonitor.com/admin/login");
-        
+        String filePath = "suresh.jpg";
+        // Perform login
         driver.findElement(By.xpath("//input[@placeholder='Email Address']")).sendKeys("track2024@blondmail.com");
         driver.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys("@Mahantesh123");
         driver.findElement(By.xpath("//button[text()='Login']")).click();
 
-        
-     String projectPath=   System.getProperty("user.dir");
-        
         // Navigate to the menu
         WebElement menuIcon = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='flex gap-4 2xl:gap-8 items-center']")));
         menuIcon.click();
@@ -47,12 +45,25 @@ public class AlertHandle {
         // Wait and ensure the file input element is clickable
         WebElement fileInput = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@role=\"menuitem\"]//img[@alt=\"edit\"]")));
         fileInput.click();
-        Thread.sleep(5000);  // Adjust the wait time if needed
-        fileInput.sendKeys(projectPath+"\\Files\\suresh.jpg");
-      
+       
+        Thread.sleep(1000);  // Adjust the wait time if needed
 
         
-       
+        char[] name=filePath.toCharArray();
+        // Create Robot instance to simulate keyboard actions
+        Robot robot = new Robot();
+
+        // Type the file path into the file dialog
+        
+
+        for (char C : name) {
+            robot.keyPress(KeyEvent.getExtendedKeyCodeForChar(C));
+            robot.keyRelease(KeyEvent.getExtendedKeyCodeForChar(C));
+        }
+
+        // Press "Enter" to select the file
+        robot.keyPress(KeyEvent.VK_ENTER);
+        robot.keyRelease(KeyEvent.VK_ENTER);
 
         // Wait and verify upload success
         Thread.sleep(2000);  // Wait for upload to finish or success message
@@ -64,10 +75,7 @@ public class AlertHandle {
         alert.accept();
         
         
-        driver.findElement(By.xpath("//button[text()=\"Save Changes\"]")).click()
-;    
+        driver.findElement(By.xpath("//button[text()=\"Save Changes\"]")).click();
+    
     }
-    
-    
- 
 }
